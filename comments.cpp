e333a550b0808bfca5533b47292c30fd72d39e39
KOT-B-MEIIIKE <42624136+KOT-B-MEIIIKE@users.noreply.github.com>
2018-09-05 22:18:33 +0300

    Update comments.cpp

--- comments.cpp
+++ comments.cpp
@@ -1,9 +1,9 @@
 // comments.cpp
 // demonstrates comments
-#include <iostream>			// preprocessor directive
+#include <iostream>		// preprocessor directive
 using namespace std;		// "using" directive
-int main()					// function name "main"
-{							// start function body
+int main()			// function name "main"
+{				// start function body
 	cout << "Every age has a language of its own\n"; // statement
 	return 0;               // statement
-}							// end function body
+}				// end function body
